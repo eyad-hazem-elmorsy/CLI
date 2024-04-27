6b6d0ad77ebb890e1a324f491747843ccda94f4f
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-04-27 20:36:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { Command } from 'commander';
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+const program = new Command();
+
+program
+  .name('GitHub-APIs')
+  .description('CLI to get the repositories of a single user')
+  .version('1.0.0');
+
+program
+.command('get')
+.description('get a repo')
+.action(() => {
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter the username:\n'
+        }
+        ])
+        .then((answer) => {
+            const url = 'https://api.github.com/users/' + answer.username + '/repos';
+            console.log(url);
+            fetch(url).then((ret) => {
+                return ret.json();
+            }).then((ret) => {
+                const repos = ret.map(repo => repo.name);
+                fs.writeFile('./repos/' + answer.username + '.json', JSON.stringify(repos), 'utf8', () => {
+                    console.log("Repo has been added");
+                })
+            })
+        }) 
+});
+
+program.parse(process.argv);
